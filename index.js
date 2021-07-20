929cfe95ea6b471f36ae6ee6b8db806e97a2dced
Nastya <asekerich99@gmail.com>
2021-07-20 10:53:56 +0300

    feat: add GET endpoint

--- index.js
+++ index.js
@@ -4,32 +4,45 @@ const fs = require('fs');
 const port = 3000;
 
 const requestHandler = (request, response) => {
-  if (request.method === 'POST') {
-    let data = '';
+  if (request.url === '/nodejs') {
+    if (request.method === 'POST') {
+      let data = '';
 
-    request.on('data', (value) => {
-      data += value;
-    });
+      request.on('data', (value) => {
+        data += value;
+      });
 
-    request.on('end', () => {
-      if (data) {
-        fs.writeFileSync('data.json', data);
+      request.on('end', () => {
+        if (data) {
+          fs.writeFileSync('data.json', data);
 
-        response.statusCode = 200;
-        response.statusMessage = 'Success';
-        response.setHeader('Content-Type', 'text/plain');
-        response.write(
-          `Status code: ${response.statusCode} (${response.statusMessage})`
-        );
-        response.end();
-      } else {
-        response.writeHead(400, { 'Content-Type': 'text/plain' });
-        response.write(
-          `Status code: ${response.statusCode} (${response.statusMessage})`
-        );
-        response.end();
-      }
-    });
+          response.statusCode = 200;
+          response.statusMessage = 'Success';
+          response.setHeader('Content-Type', 'text/plain');
+          response.write(
+            `Status code: ${response.statusCode} (${response.statusMessage})`
+          );
+          response.end();
+        } else {
+          response.writeHead(400, { 'Content-Type': 'text/plain' });
+          response.write(
+            `Status code: ${response.statusCode} (${response.statusMessage})`
+          );
+          response.end();
+        }
+      });
+    }
+
+    if (request.method === 'GET') {
+      fs.readFile('data.json', (err, data) => {
+        if (err) {
+          throw err
+        }
+        response.end(data);
+      });
+    }
+  } else {
+    response.end('No data');
   }
 };
 
